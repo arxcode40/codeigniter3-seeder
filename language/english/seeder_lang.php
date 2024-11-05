7e8d05c2dd11f6b0677fefef458278fe170f4ee5
arxcode40 <arxcode40@gmail.com>
2024-11-05 07:50:44 +0700

    add indonesian translations

--- language/english/seeder_lang.php
+++ language/english/seeder_lang.php
@@ -2,6 +2,6 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $lang['seeder_not_enabled'] = 'Seeders has been loaded but is disabled or set up incorrectly.';
-$lang['seeder_file_doesnt_exist'] = 'The seeder file "%s" could not be found';
+$lang['seeder_file_doesnt_exist'] = 'The seeder file "%s" could not be found.';
 $lang['seeder_class_doesnt_exist'] = 'The seeder class "%s" could not be found.';
-$lang['seeder_missing_run_method'] = 'The migration class "%s" is missing an "run" method.';
+$lang['seeder_missing_run_method'] = 'The seeder class "%s" is missing a "run" method.';
---
+++ language/indonesian/seeder_lang.php
@@ -0,0 +1,7 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['seeder_not_enabled'] = 'Seeders telah dimuat tetapi dinonaktifkan atau disiapkan secara tidak benar.';
+$lang['seeder_file_doesnt_exist'] = 'File seeder "%s" tidak dapat ditemukan.';
+$lang['seeder_class_doesnt_exist'] = 'Kelas seeder "%s" tidak dapat ditemukan.';
+$lang['seeder_missing_run_method'] = 'Kelas seeder "%s" tidak memiliki metode "run".';
